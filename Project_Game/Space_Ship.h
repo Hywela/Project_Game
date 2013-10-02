decfc4aa17d9836a0fbbce8d231288120639b95d
Aleksander Fimreite <Aleks_F92@hotmail.com>
2013-10-02 14:44:30 +0200

    Build mode v1
    This is a "playable" version of the build mode.

--- Project_Game/Space_Ship.h
+++ Project_Game/Space_Ship.h
@@ -3,24 +3,26 @@
 #include "Module.h"
 #include "Module_Gun.h"
 #include "Module_Shield.h"
-
-
-
-const int SHIP_WIDTH = 3;
-const int SHIP_HEIGTH = 3;
+#include "Constants.h"
 
 
 
 class Space_Ship
 {
 private:
-	Hull *hull_layer[SHIP_HEIGTH][SHIP_WIDTH];
-	Module *module_layer[SHIP_HEIGTH][SHIP_WIDTH];
+	SDL_Rect *position;	//Where the ship is position (top-left corner)
+	Hull *hull_layer[SHIP_HEIGHT][SHIP_WIDTH];
+	Module *module_layer[SHIP_HEIGHT][SHIP_WIDTH];
+	int selected_hull;
 
 public:
 	Space_Ship();
+	Space_Ship(SDL_Renderer *ren);
 	~Space_Ship();
 
 	void draw(SDL_Renderer *ren);
+	bool buildHull(SDL_Renderer *ren);
+	void swapHull(SDL_Renderer *ren, int x, int y, int type);
+	bool buildModules(SDL_Renderer *ren);
 };
 
